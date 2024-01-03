d3e6ac32c2f8043ded6a0eb1e730076a8aa8bbbc
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-01-03 15:45:30 +0100

    Style header to be more responsive friendly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { createMobileMenu } from "./mobile-menu.js";
 import { createDesktopMenu } from "./desktop-menu.js";
 
-const mediaQuery = window.matchMedia('(max-width: 1150px)')
+const mediaQuery = window.matchMedia('(max-width: 1000px)')
 
 function handleTabletChange(evt) {
     if (evt.matches) {
